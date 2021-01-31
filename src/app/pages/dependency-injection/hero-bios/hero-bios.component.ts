816c0c540e8da37c280f423ea4321ac18157eb9a
Wilfred <wilfred05777@gmail.com>
2021-02-01 07:56:05 +0800

    correction file management in DI

---
+++ src/app/pages/dependency-injection/hero-bios/hero-bios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-bios',
+  templateUrl: './hero-bios.component.html',
+  styleUrls: ['./hero-bios.component.scss']
+})
+export class HeroBiosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
