40c03237f72a4876bdb4f4baa18b065a83edb705
Wilfred <wilfred05777@gmail.com>
2021-02-01 07:58:55 +0800

    circular Dependency folder correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { CircuralDependencyInjectionComponent } from './pages/circural-dependency-injection/circural-dependency-injection.component';
 import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
 import { HeroBiosComponent } from './pages/hero-bios/hero-bios.component';
+import { NG0200CircularDependencyComponent } from './pages/dependency-injection/ng0200-circular-dependency/ng0200-circular-dependency.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HeroBiosComponent } from './pages/hero-bios/hero-bios.component';
     CircuralDependencyInjectionComponent,
     DependencyInjectionComponent,
     HeroBiosComponent,
+    NG0200CircularDependencyComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/pages/dependency-injection/ng0200-circular-dependency/ng0200-circular-dependency.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng0200-circular-dependency',
+  templateUrl: './ng0200-circular-dependency.component.html',
+  styleUrls: ['./ng0200-circular-dependency.component.scss']
+})
+export class NG0200CircularDependencyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
