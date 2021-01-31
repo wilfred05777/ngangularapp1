29baae2efeacba0c8e69fdfb030be1045b650a22
Wilfred <wilfred05777@gmail.com>
2021-02-01 07:54:16 +0800

    cleanup wrong folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CirucalDependencyInjectionComponent } from './pages/cirucal-dependency-injection/cirucal-dependency-injection.component';
 import { CircuralDependencyInjectionComponent } from './pages/circural-dependency-injection/circural-dependency-injection.component';
 import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
 import { HeroBiosComponent } from './pages/hero-bios/hero-bios.component';
@@ -13,7 +12,6 @@ import { HeroBiosComponent } from './pages/hero-bios/hero-bios.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    CirucalDependencyInjectionComponent,
     CircuralDependencyInjectionComponent,
     DependencyInjectionComponent,
     HeroBiosComponent,
