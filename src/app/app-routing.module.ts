8dc08a00d96fdc16561e7edde7f9dc2c95395617
Wilfred <wilfred05777@gmail.com>
2021-02-01 07:43:53 +0800

    inital routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
+import { HeroBiosComponent } from './pages/hero-bios/hero-bios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeroBiosComponent },
+  { path: 'di', component: DependencyInjectionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
