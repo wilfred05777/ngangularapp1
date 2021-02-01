beb12cb8ae3bedd26461c88fbd83517455fa82dc
Wilfred <wilfred05777@gmail.com>
2021-02-01 08:04:03 +0800

    policy and home pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
 import { NG0200CircularDependencyComponent } from './pages/dependency-injection/ng0200-circular-dependency/ng0200-circular-dependency.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PolicyComponent } from './pages/policy/policy.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { NG0200CircularDependencyComponent } from './pages/dependency-injection/
     HeaderComponent,
     DependencyInjectionComponent,
     NG0200CircularDependencyComponent,
+    HomeComponent,
+    PolicyComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
