48f7d023c18fa15ce2e1dda93e48533e6ce2ebf7
Wilfred <wilfred05777@gmail.com>
2021-02-01 08:42:20 +0800

    loggers services

--- src/app/pages/dependency-injection/hero-bios/hero-bios.component.ts
+++ src/app/pages/dependency-injection/hero-bios/hero-bios.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroService } from 'src/app/services/hero.service';
 
 @Component({
   selector: 'app-hero-bios',
   templateUrl: './hero-bios.component.html',
   styleUrls: ['./hero-bios.component.scss'],
-  providers: [HeroService],
 })
 export class HeroBiosComponent implements OnInit {
   constructor() {}
--- src/app/services/hero-cache.service.ts
+++ src/app/services/hero-cache.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Hero } from '../models/hero.model';
+import { HeroService } from './hero.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroCacheService {
+  hero: Hero;
+  constructor(private heroService: HeroService) {}
 
-  constructor() { }
+  fetchCachedHero(id: number) {
+    if (!this.hero) {
+      this.hero = this.heroService.getHeroById(id);
+    }
+    return this.hero;
+  }
 }
