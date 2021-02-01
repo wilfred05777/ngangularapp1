48a4fa113fa8a0af138d65019fd9e062d0f46303
Wilfred <wilfred05777@gmail.com>
2021-02-01 08:00:55 +0800

    cleanup wrong forlder structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CircuralDependencyInjectionComponent } from './pages/circural-dependency-injection/circural-dependency-injection.component';
 import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
-import { HeroBiosComponent } from './pages/hero-bios/hero-bios.component';
 import { NG0200CircularDependencyComponent } from './pages/dependency-injection/ng0200-circular-dependency/ng0200-circular-dependency.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CircuralDependencyInjectionComponent,
     DependencyInjectionComponent,
-    HeroBiosComponent,
     NG0200CircularDependencyComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
