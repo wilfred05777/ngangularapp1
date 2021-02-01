0ede1547ac6244af21da84e6a8a7ee46bbc7c6de
Wilfred <wilfred05777@gmail.com>
2021-02-01 09:21:28 +0800

    save point

--- src/app/pages/dependency-injection/dependency-injection.component.ts
+++ src/app/pages/dependency-injection/dependency-injection.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggerService } from 'src/app/services/logger.service';
+import { UserContextService } from 'src/app/services/user-context.service';
 
 @Component({
   selector: 'app-dependency-injection',
   templateUrl: './dependency-injection.component.html',
-  styleUrls: ['./dependency-injection.component.scss']
+  styleUrls: ['./dependency-injection.component.scss'],
 })
 export class DependencyInjectionComponent implements OnInit {
+  private userId = 1;
 
-  private userId: = 1;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(logger: LoggerService, public userContext: UserContextService) {
+    //stackblitz.com/edit/angular-vnhgpq?file=src%2Fapp%2Fapp.component.ts
+    // https: userContext.loadUser(this.userId);
   }
 
+  ngOnInit(): void {}
 }
--- src/app/services/user-context.service.ts
+++ src/app/services/user-context.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserContextService {
-
-  constructor() { }
+  constructor() {}
 }
