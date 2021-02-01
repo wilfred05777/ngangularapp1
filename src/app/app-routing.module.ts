1d2fa0d70f25a4b7926dba9d336963f7529eb999
Wilfred <wilfred05777@gmail.com>
2021-02-01 09:11:06 +0800

    DI cookbook reference

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
 import { HeroBiosComponent } from './pages/dependency-injection/hero-bios/hero-bios.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HeroBiosComponent },
+  { path: '', component: HomeComponent },
+
   { path: 'di', component: DependencyInjectionComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { HeroService } from './services/hero.service';
     PolicyComponent,
     HeroBiosComponent,
     HeroBioComponent,
+    DependencyInjectionComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [AuthService, HeroService, HeroCacheService],
