e543290e642e38bceca90b01fe664ae25eefe4ec
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-02-02 10:25:44 +0800

    add store routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DependencyInjectionComponent } from './pages/dependency-injection/dependency-injection.component';
 import { HeroBiosComponent } from './pages/dependency-injection/hero-bios/hero-bios.component';
 import { HomeComponent } from './pages/home/home.component';
+import { StoreComponent } from './pages/store/store.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-
   { path: 'di', component: DependencyInjectionComponent },
+  { path: 'store', component: StoreComponent },
 ];
 
 @NgModule({
