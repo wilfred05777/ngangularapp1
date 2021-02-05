b949c45b0733fa54159482349dc30fd8ac37a4eb
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-02-05 09:05:01 +0800

    Added HttpClient Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +30,7 @@ import { DashboardComponent } from './pages/tour-of-heroes/components/dashboard/
 @NgModule({
   declarations: [
     AppComponent,
+    HttpClientModule,
     HeaderComponent,
     DependencyInjectionComponent,
     NG0200CircularDependencyComponent,
